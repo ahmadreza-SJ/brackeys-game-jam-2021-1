b78a75134472e7e00aa2f4ac112a5b07652066a4
Amirreza Amouie <aamouie01@gmail.com>
2021-02-16 12:12:11 +0330

    add some dummy prefab instantiation without any rules

---
+++ brackeys20211/Assets/Scripts/ChunkGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkGenerator : MonoBehaviour
+{
+
+    public GameObject positive;
+    public GameObject negative;
+    public GameObject comet;
+
+    private int XSIZE = 7;
+    private int YSIZE = 10;
+    private int XDIFF = 4;
+    private int YDIFF = 3;
+    private int[,] BOARD;
+
+    void Awake()
+    {
+        BOARD = new int[XSIZE, YSIZE];
+        
+        Generate();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Generate()
+    {
+        for (int y=0; y<YSIZE; y++)
+        {
+            for (int x=0; x<XSIZE; x++)
+            {
+                Instantiate(positive, new Vector3(x-XDIFF, y-YDIFF, 0), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ brackeys20211/Assets/Scripts/MapController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject chunkPrefab;
+    void Start()
+    {
+        Instantiate(chunkPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
