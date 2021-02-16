0a0f12040e602e007381f937e32eefe8b4333ea7
Ahmadreza Sanatjou <a.reza.sanatjoo@gmail.com>
2021-02-17 01:43:53 +0330

    Implementing player movement and colision with the walls

---
+++ brackeys20211/Assets/Scripts/Player/MovementBoundaries.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class MovementBoundaries : MonoBehaviour
+    {
+        private Camera MainCam;
+        private Rigidbody2D cameraRigidbody;
+
+        private void AddCollider(float x, float y)
+        {
+            EdgeCollider2D col = MainCam.gameObject.AddComponent<EdgeCollider2D>();
+            List<Vector2> points = new List<Vector2>();
+            points.Add(new Vector2(x, -y));
+            points.Add(new Vector2(x, y));
+            col.SetPoints(points);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            MainCam = Camera.main;
+            cameraRigidbody = MainCam.gameObject.AddComponent<Rigidbody2D>();
+            cameraRigidbody.gravityScale = 0;
+            AddCollider(GameManager.Instance.screenHalfWidth, GameManager.Instance.screenHalfHeight);
+            AddCollider(-GameManager.Instance.screenHalfWidth, GameManager.Instance.screenHalfHeight);
+        }
+    }
+}
---
+++ brackeys20211/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 40));
+        }
+
+
+    }
+}
